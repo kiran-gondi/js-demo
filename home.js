9aec4e83bdfb2764ccb30abdf2303dba1822f47d
kiran.gondi <kiran.gondi@planview.com>
2023-12-09 21:32:09 +0530

    Chapter 3 - Types & Operators

--- home.js
+++ home.js
@@ -1,10 +1,58 @@
-let welcome = 'WelCome';
+// let person = {
+//     firstName: 'John',
+//     lastName: 'Adams'
+// };
+// showMessage(person.firstName);
 
-let price = 45.54,
-    name = 'Hiking Boots12',
-    discounted = false;
+// let saved;
+// saved = null;
+// showMessage(saved);
+// console.log(saved);
 
-showMessage(welcome);
+// let saved;
+// showMessage(saved);
+// console.log(saved);
+
+// let amount = Number.parseFloat("123.12A");
+// showMessage(amount);
+
+// let amount = Number.parseFloat("A123.12");
+// showMessage(typeof amount);
+
+// let amt = 123;
+// amt = amt.toString();
+// showMessage(typeof amt);
+
+//let message = 'Hello \'World\'';
+// let name = 'kg';
+// let message = `Hello ${name}`;
+// showMessage(message);
+
+// let price = -20;
+// showMessage(price);
+
+// let price = 1.1 + 1.3;
+// showMessage(price);
+
+// let price = 20.99;
+// showMessage(typeof price);
+
+// let price = 3 + 2 * 2;
+// let price = (3 + 2) * 2;
+// showMessage(price);
 
 //var, let & naming variables
+
+// let welcome = 'WelCome';
+
+// let price = 45.54,
+//     name = 'Hiking Boots12',
+//     discounted = false;
+
+// showMessage(welcome);
+
+
+
+
+
 
